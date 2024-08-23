810ddc0d01ceb3960965f88badf5907289699622
JacopoLombardi <lombajacopo@gmail.com>
2024-08-24 00:32:14 +0200

    [add] completed Travel_detail_info mobile

---
+++ src/components/partials/Travel Detail Sections/Travel_detail_info.vue
@@ -0,0 +1,181 @@
+
+<script>
+import Accordion_Travel_detail_info from "./partial Travel_detail/Accordion_Travel_detail_info.vue"
+
+export default {
+  components: {
+    Accordion_Travel_detail_info
+  },
+  props: {
+    data: Object,
+  },
+  computed: {
+    // funzione che abbrevia la chiamata dei dati nel template
+    detail() {
+      return this.data?.detail;
+    }
+  },
+};
+</script>
+
+
+
+
+<template>
+  <div v-if="data">
+
+    <!-- Jumbotron -->
+    <div class="_jumbotron">
+      <img :src="detail.img" :alt="data.title">
+      <h1>{{ data.title }}</h1>
+    </div>
+
+
+    <!-- Info -->
+    <div class="_info">
+      <h2>{{ detail.title }}</h2>
+
+
+      <!-- Badge -->
+      <div class="_badge d-flex flex-wrap">
+        <div
+          class="badge_box mt-2"
+          v-for="(badge, index) in detail.badge"
+          :key="index"
+        >
+          <p>{{ badge }}</p>
+        </div>
+      </div>
+
+
+      <!-- Text -->
+      <div class="_text">
+        <p
+          v-for="(paragraph, index) in detail.paragraph"
+          :key="index"
+          v-html="paragraph.text"
+        ></p>
+      </div>
+
+
+      <!-- Add Flight -->
+      <div class="add_flight">
+        <div class="_title d-flex align-items-center">
+          <i class="fa-solid fa-plane"></i>
+          <h4>{{ detail.flight.title }}</h4>
+        </div>
+        <p v-html="detail.flight.text"></p>
+      </div>
+
+
+      <!-- Componente Accordion -->
+      <Accordion_Travel_detail_info
+        :detail="detail"
+      />
+
+    </div>
+
+  </div>
+</template>
+
+
+
+
+<style scoped lang="scss">
+
+/* Jumbotron */
+._jumbotron {
+  position: relative;
+
+  img {
+    object-fit: cover;
+    height: 400px;
+    width: 100vw;
+    filter: brightness(80%);
+  }
+
+  h1 {
+    color: white;
+    text-align: center;
+    width: 100%;
+    font-size: clamp(2rem, 12vw, 4.5rem);
+    font-weight: 700;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+
+
+
+/* Info */
+._info {
+  padding: 50px 1rem 70px 1rem;
+  background-color: rgba(247, 247, 247, 0.935);
+
+  h2 {
+    color: orangered;
+    font-weight: 800;
+    font-size: 22px;
+    margin-bottom: 20px;
+    text-transform: uppercase;
+  }
+
+  ._badge {
+    margin-bottom: 30px;
+
+    .badge_box {
+      background-color: rgba(230, 221, 255, 0.782);
+      color: rgb(107, 44, 255);
+      padding: 8px 15px;
+      border-radius: 20px;
+      font-size: 13px;
+      margin-right: 10px;
+    }
+  }
+
+  ._text {
+    font-size: 14px;
+    line-height: 110%;
+
+    p {
+      margin-bottom: 15px;
+    }
+  }
+
+
+
+  /* Flight */
+  .add_flight {
+    background-color: rgba(228, 218, 255, 0.798);
+    color: rgb(107, 44, 255);
+    padding: 20px;
+    margin-top: 40px;
+    border-radius: 20px;
+
+    ._title {
+      border-bottom: 1px solid rgba(107, 44, 255, 0.446);
+      padding-bottom: 10px;
+      margin-bottom: 10px;
+
+      i {
+        margin-right: 10px;
+        font-size: 20px;
+      }
+
+      h4 {
+        margin: 0;
+        font-size: 18px;
+        font-weight: 700;
+      }
+    }
+
+    p {
+      font-size: 13px;
+    }
+  }
+
+}
+
+</style>
---
+++ src/components/partials/Travel Detail Sections/partial Travel_detail/Accordion_Travel_detail_info.vue
@@ -0,0 +1,212 @@
+
+<script>
+
+export default {
+  props: {
+    detail: Object
+  },
+  data(){
+    return{
+
+    };
+  },
+
+  methods:{
+
+  },
+};
+</script>
+
+
+
+
+<template>
+  <!-- Accordion -->
+  <div class="accordion" id="accordionExample">
+
+    <!-- Included -->
+    <div class="accordion-item mb-3">
+      <h2 class="accordion-header m-0">
+        <button
+          class="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseOne"
+          aria-expanded="true"
+          aria-controls="collapseOne"
+        >
+          <i class="fa-regular fa-circle-check"></i>
+          Cosa è incluso
+        </button>
+      </h2>
+      <div
+        id="collapseOne"
+        class="accordion-collapse collapse show"
+        data-bs-parent="#accordionExample"
+      >
+        <div class="accordion-body">
+          <ul>
+            <li
+              v-for="(item, index) in detail.included"
+              :key="index"
+              class="_box d-flex align-items-center"
+            >
+              <div class="icon_box">
+                <i :class="item.icon"></i>
+              </div>
+              <p>{{ item.text }}</p>
+            </li>
+          </ul>
+
+
+          <!-- Not Included -->
+          <h3>La quota viaggio non comprende</h3>
+          <ul>
+            <li
+              v-for="(item, index) in detail.notIncluded"
+              :key="index"
+              class="d-flex align-items-center"
+            >
+              <div class="icon_box">
+                <i class="fa-solid fa-xmark text-danger"></i>
+              </div>
+              <p>{{ item }}</p>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+
+    </div>
+
+
+    <!-- Things to Know -->
+    <div class="accordion-item things_to_know">
+      <h2 class="accordion-header m-0">
+        <button
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseTwo"
+          aria-expanded="false"
+          aria-controls="collapseTwo"
+        >
+          <i class="fa-regular fa-circle-question"></i>
+          Cose da sapere
+        </button>
+      </h2>
+      <div
+        id="collapseTwo"
+        class="accordion-collapse collapse"
+        data-bs-parent="#accordionExample"
+      >
+        <div class="accordion-body">
+          <ul>
+            <li
+              v-for="(item, index) in detail.thingsToKnow"
+              :key="index"
+            >
+              <h4>{{ item.title }}</h4>
+              <p>{{ item.text }}</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+
+
+
+
+
+<style lang="scss" scoped>
+
+/* Accordion */
+.accordion {
+    --bs-accordion-active-color: black;
+    margin-top: 30px;
+
+    .accordion-item {
+      border-radius: 20px;
+      border: 1px solid rgb(224, 224, 224);
+      padding: 10px 10px;
+
+      .accordion-button {
+        position: relative;
+        font-size: 16px;
+        border-radius: 20px;
+        font-weight: 700;
+
+        &:not(.collapsed) {
+          background-color: transparent;
+          box-shadow: none;
+        }
+
+        &:focus {
+          box-shadow: none;
+        }
+
+        i {
+          margin-right: 10px;
+          font-size: 20px;
+          color: rgb(33, 210, 33);
+        }
+      }
+
+      .accordion-body {
+        padding: 5px 15px 0 15px;
+
+        ul {
+          li {
+            margin-bottom: 15px;
+
+            .icon_box {
+              display: flex;
+              width: 30px;
+              margin-right: 5px;
+
+              i {
+                text-align: center;
+                color: rgb(58, 58, 58);
+                width: 100%;
+                font-size: 1.2rem;
+              }
+            }
+
+            p {
+              font-size: 13px;
+              line-height: 120%;
+              width: 80%;
+            }
+          }
+        }
+
+        h3 {
+          font-size: 17px;
+          font-weight: 700;
+          margin-top: 30px;
+          margin-bottom: 15px;
+        }
+      }
+    }
+
+    .things_to_know {
+      .accordion-button {
+        i {
+          color: red;
+        }
+      }
+
+      h4 {
+        font-size: 14px;
+        margin: 0;
+        font-weight: 700;
+      }
+    }
+  }
+
+</style>
