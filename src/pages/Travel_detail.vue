ff5b7a43598887278365a060abe2b58b4a4453e6
JacopoLombardi <lombajacopo@gmail.com>
2024-08-23 19:56:03 +0200

    [add] maked section _included in Travel_detail

--- src/pages/Travel_detail.vue
+++ src/pages/Travel_detail.vue
@@ -41,11 +41,7 @@ export default {
 
         <!-- badge -->
         <div class="_badge d-flex flex-wrap">
-          <div
-            class="badge_box mt-2"
-            v-for="item in slideData.detail.badge"
-            :key="item"
-          >
+          <div class="badge_box mt-2" v-for="item in slideData.detail.badge" :key="item">
             <p>{{ item.name }}</p>
           </div>
         </div>
@@ -53,11 +49,7 @@ export default {
 
         <!-- text -->
         <div class="_text">
-          <p
-            v-for="item in slideData.detail.paragraph"
-            :key="item"
-            v-html="item.text"
-          ></p>
+          <p v-for="item in slideData.detail.paragraph" :key="item" v-html="item.text"></p>
         </div>
 
 
@@ -67,16 +59,161 @@ export default {
             <i class="fa-solid fa-plane"></i>
             <h4>{{ slideData.detail.flight.title }}</h4>
           </div>
-          <p 
-            v-html="slideData.detail.flight.text"
-          ></p>
+          <p v-html="slideData.detail.flight.text"></p>
         </div>
 
 
+        <!-- accordion -->
+        <div class="_accordions">
 
+
+          <div class="accordion" id="accordionExample">
+
+            <!-- included -->
+            <div class="accordion-item mb-3">
+              <h2 class="accordion-header m-0">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                  aria-expanded="true" aria-controls="collapseOne">
+                  <i class="fa-regular fa-circle-check"></i>
+                  Cosa è incluso
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+
+                  <ul>
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-regular fa-moon"></i>
+                      </div>
+                      <p>7 notti in hotel</p>
+                    </li>
+
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-mug-saucer"></i>
+                      </div>
+                      <p>Colazione</p>
+                    </li>
+
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-location-dot"></i>
+                      </div>
+                      <p>Esperienza inclusa: full day Phuket tour</p>
+                    </li>
+
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-regular fa-circle-user"></i>
+                      </div>
+                      <p>Coach in lingua italiana</p>
+                    </li>
+
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-bus-simple"></i>
+                      </div>
+                      <p>Trasferimenti da/per aeroporto (se acquisti il volo con noi)</p>
+                    </li>
+
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-gift"></i>
+                      </div>
+                      <p>Braccialetto da Dao Ethical Gifts</p>
+                    </li>
+
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-headset"></i>
+                      </div>
+                      <p>Assistenza Utravel</p>
+                    </li>
+
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-tablet-screen-button"></i>
+                      </div>
+                      <p>App dedicata</p>
+                    </li>
+
+                    <li class="_box d-flex align-items-center">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-users"></i>
+                      </div>
+                      <p>Assicurazione base: copertura spese mediche fino a 5.000€ e fino a 1.000€ per il bagaglio oltre ad un massimo di 310€ per acquisti di prima necessità</p>
+                    </li>
+                  </ul>
+
+
+                  <h3>La quota viaggio non comprende</h3>
+                  <ul>
+                    <li class="d-flex">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-xmark text-danger"></i>
+                      </div>
+                      <p>Volo a/r (puoi aggiungerlo in fase di prenotazione)</p>
+                    </li>
+
+                    <li class="d-flex">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-xmark text-danger"></i>
+                      </div>
+                      <p>Pranzi e cene</p>
+                    </li>
+
+                    <li class="d-flex">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-xmark text-danger"></i>
+                      </div>
+                      <p>Tassa di soggiorno</p>
+                    </li>
+
+                    <li class="d-flex">
+                      <div class="icon_box">
+                        <i class="fa-solid fa-xmark text-danger"></i>
+                      </div>
+                      <p>Tutto quello che non è indicato in "Cosa è incluso"</p>
+                    </li>
+                  </ul>
+
+                </div>
+              </div>
+            </div>
+
+
+            <!-- Things to know -->
+            <div class="accordion-item">
+              <h2 class="accordion-header m-0">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Cose da sapere
+                </button>
+              </h2>
+              <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <ul>
+                    <li>ssasd</li>
+                    <li>ssasd</li>
+                    <li>ssasd</li>
+                    <li>ssasd</li>
+                    <li>ssasd</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+
+
+        </div>
+
+        
       </div>
 
-  
+
     </div>
 
   </div>
@@ -87,6 +224,9 @@ export default {
 <style lang="scss" scoped>
 
 .travel_detail {
+
+
+
 
 
   ._jumbotron{
@@ -111,6 +251,8 @@ export default {
       transform: translate(-50%, -50%);
     }
   }
+
+
 
 
 
@@ -154,6 +296,9 @@ export default {
 
 
 
+
+
+
     .add_flight {
       background-color: rgba(228, 218, 255, 0.798);
       color: rgb(107, 44, 255);
@@ -182,6 +327,88 @@ export default {
         font-size: 13px;
       }
     }
+
+
+
+
+
+
+    ._accordions {
+      margin-top: 30px;
+
+      .accordion {
+
+        .accordion-item {
+          border-radius: 20px;
+          border: 1px solid rgb(224, 224, 224);
+          padding: 10px 10px;
+
+          .accordion-button {
+            position: relative;
+            font-size: 16px;
+            border-radius: 20px;
+            font-weight: 700;
+
+            &:not(.collapsed) {
+              background-color: transparent;
+              box-shadow: none;
+            }
+
+            &:focus {
+              box-shadow: none;
+            }
+
+            i {
+              color: rgb(33, 219, 33);
+              margin-right: 10px;
+              font-size: 20px;
+            }
+          }
+
+          .accordion-body {
+            padding: 10px 15px 0 15px;
+
+            ul {
+              li {
+                margin-bottom: 15px;
+
+                .icon_box {
+                  display: flex;
+                  width: 30px;
+                  margin-right: 10px;
+
+                  i {
+                    text-align: center;
+                    color: rgb(58, 58, 58);
+                    width: 100%;
+                    font-size: 1.2rem;
+                  }
+                }
+               
+                p {
+                  font-size: 13px;
+                  line-height: 140%;
+                }
+                
+              }
+            }
+
+            h3 {
+              font-size: 17px;
+              font-weight: 700;
+              margin-top: 30px;
+              margin-bottom: 15px;
+            }
+
+          }
+        }
+      }
+    }
+
+
+
+
+
 
 
 
